732a2655d2e0515a16b0f78588d23c7732bd9414
raphael <raphael.stary@gmail.com>
2018-09-15 12:44:25 -0700

    refactoring to update buffers w/webgl + use pre gen buffer data

--- code-src-tx/main.js
+++ code-src-tx/main.js
@@ -1,3 +1,5 @@
+import * as SubImage from '../code-gen/SubImage.js';
+
 if (window.Windows)
     console.log(`I'm running on Windows 😎`);
 
@@ -5,12 +7,20 @@ Promise.all([
 
     new Promise(resolve => window.onload = resolve),
 
-    fetch('../asset-gen/atlas_720_0.json')
+    fetch('../asset-gen/sprite-dimensions_720.h5')
         .then(response => {
             if (response.ok)
-                return response.json();
+                return response.arrayBuffer();
 
-            throw  new Error('could not fetch json');
+            throw new Error('could not fetch sprite-dimensions');
+        }),
+
+    fetch('../asset-gen/sub-images_720.h5')
+        .then(response => {
+            if (response.ok)
+                return response.arrayBuffer();
+
+            throw new Error('could not fetch sub-image-data');
         }),
 
     fetch('../asset-gen/atlas_720_0.png')
@@ -18,7 +28,7 @@ Promise.all([
             if (response.ok)
                 return response.blob();
 
-            throw  new Error('could not fetch image');
+            throw new Error('could not fetch texture-atlas');
         })
         .then(blob => {
             return new Promise(resolve => {
@@ -30,68 +40,28 @@ Promise.all([
 ])
     .catch(error => console.log(error))
     .then(values => {
-        const json = values[1];
-        const img = values[2];
+        const dimensions = new Uint32Array(values[1]);
+        const subImages = new Float32Array(values[2]);
+        const img = values[3];
 
-        const aceOfSpades = json.frames['card-SA'].frame;
-        const aceOfHearts = json.frames['card-HA'].frame;
-        const aceOfClubs = json.frames['card-CA'].frame;
-        const aceOfDiamonds = json.frames['card-DA'].frame;
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array([1280 / 2, 720 / 2, 1]));
 
-        const w = json.meta.size.w;
-        const h = json.meta.size.h;
+        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+        gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array([0, 1, 1, 0.4]));
 
-        const subImages = [
-            aceOfSpades.x / w, aceOfSpades.y / h, aceOfSpades.w / w, aceOfSpades.h / h,
-            aceOfHearts.x / w, aceOfHearts.y / h, aceOfHearts.w / w, aceOfHearts.h / h,
-            aceOfClubs.x / w, aceOfClubs.y / h, aceOfClubs.w / w, aceOfClubs.h / h,
-            aceOfDiamonds.x / w, aceOfDiamonds.y / h, aceOfDiamonds.w / w, aceOfDiamonds.h / h
-        ];
-        const subImageBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, transformsBuffer);
+        gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array([1, 5]));
+
+        const dimIdx = SubImage.CARD_C2 * 2;
+        gl.bindBuffer(gl.ARRAY_BUFFER, dimensionsBuffer);
+        gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array([dimensions[dimIdx], dimensions[dimIdx + 1]]));
+
+        const subImgIdx = SubImage.CARD_C2 * 4;
         gl.bindBuffer(gl.ARRAY_BUFFER, subImageBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(subImages), gl.STATIC_DRAW);
-        const subImageLocation = gl.getAttribLocation(program, 'subImage');
-        gl.vertexAttribPointer(subImageLocation, 4, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(subImageLocation);
-        ext.vertexAttribDivisorANGLE(subImageLocation, 1);
+        gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(
+            [subImages[subImgIdx], subImages[subImgIdx + 1], subImages[subImgIdx + 2], subImages[subImgIdx + 3]]));
 
-        const spriteInfo = [
-            aceOfSpades.w / 2, aceOfSpades.h / 2,
-            aceOfHearts.w / 2, aceOfHearts.h / 2,
-            aceOfClubs.w / 2, aceOfClubs.h / 2,
-            aceOfDiamonds.w / 2, aceOfDiamonds.h / 2
-        ];
-        const infoBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, infoBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(spriteInfo), gl.STATIC_DRAW);
-        const infoLocation = gl.getAttribLocation(program, 'spriteInfo');
-        gl.vertexAttribPointer(infoLocation, 2, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(infoLocation);
-        ext.vertexAttribDivisorANGLE(infoLocation, 1);
-
-        const spriteProps = [
-            0.0, 1.0,
-            1.0, 1.0,
-            -Math.PI * 0.25, 1.0,
-            0.0, 1.0
-        ];
-        const propsBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, propsBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(spriteProps), gl.STATIC_DRAW);
-        const propsLocation = gl.getAttribLocation(program, 'spriteProp');
-        gl.vertexAttribPointer(propsLocation, 2, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(propsLocation);
-        ext.vertexAttribDivisorANGLE(propsLocation, 1);
-
-        const texture = gl.createTexture();
-        gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, texture);
-
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.uniform1i(texSamplerLocation, 0);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
 
         // simplest fps meter 1/3
@@ -160,11 +130,13 @@ gl.enable(gl.DEPTH_TEST);
 gl.enable(gl.BLEND);
 gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
 
+const MAX_ELEMENTS = 100;
+
 const vertexShaderSrc = `
 
 attribute vec3 position;
-attribute vec2 spriteProp;
-attribute vec2 spriteInfo;
+attribute vec2 xforms;
+attribute vec2 dimensions;
 attribute vec4 color;
 attribute vec4 subImage;
 attribute vec4 quad;
@@ -175,8 +147,8 @@ varying vec2 texCoord;
 varying vec4 texColor;
 
 void main() {
-    float tx = spriteInfo.x * quad.x;
-    float ty = spriteInfo.y * quad.y;
+    float tx = dimensions.x * quad.x;
+    float ty = dimensions.y * quad.y;
     mat4 translate = mat4(
         1.0, 0, 0, 0,
         0, 1.0, 0, 0,
@@ -184,8 +156,8 @@ void main() {
         tx, ty, 0, 1.0
     );
 
-    float c = cos(spriteProp.x);
-    float s = sin(spriteProp.x);
+    float c = cos(xforms.x);
+    float s = sin(xforms.x);
     mat4 rotate = mat4(
         c, s, 0, 0,
         -s, c, 0, 0,
@@ -193,8 +165,8 @@ void main() {
         0, 0, 0, 1.0
     );
 
-    float sx = spriteProp.y;
-    float sy = spriteProp.y;
+    float sx = xforms.y;
+    float sy = xforms.y;
     mat4 scale = mat4(
         sx, 0, 0, 0,
         0, sy, 0, 0,
@@ -265,66 +237,93 @@ if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
 
 gl.useProgram(program);
 
-const quad = [
-    -1.0, -1.0, 0.0, 1.0,
-    -1.0, 1.0, 0.0, 0.0,
-    1.0, -1.0, 1.0, 1.0,
-    1.0, 1.0, 1.0, 0.0
-];
-const quadBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, quadBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(quad), gl.STATIC_DRAW);
-const quadLocation = gl.getAttribLocation(program, 'quad');
-gl.vertexAttribPointer(quadLocation, 4, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(quadLocation);
-ext.vertexAttribDivisorANGLE(quadLocation, 0);
-
-const texSamplerLocation = gl.getUniformLocation(program, 'tex');
-
-const spritePositions = [
-    700.0, 360.0, 1.0,
-    680.0, 360.0, 3.0,
-    640.0, 360.0, 4.0,
-    600.0, 360.0, 5.0
-];
-const positionBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(spritePositions), gl.STATIC_DRAW);
-const positionLocation = gl.getAttribLocation(program, 'position');
-gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(positionLocation);
-ext.vertexAttribDivisorANGLE(positionLocation, 1);
-
-const colors = [
-    0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.4,
-    0.0, 0.0, 0.0, 0.6,
-    0.0, 0.0, 0.0, 0.8
-];
-const colorBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-const colorLocation = gl.getAttribLocation(program, 'color');
-gl.vertexAttribPointer(colorLocation, 4, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(colorLocation);
-ext.vertexAttribDivisorANGLE(colorLocation, 1);
 
 const width = 1280;
 const height = 720;
 const zNear = -0.1;
 const zFar = -100.0;
-
 const a = 2 / width;
 const b = 2 / height;
 const c = -2 / (zFar - zNear);
-
 const tz = -(zFar + zNear) / (zFar - zNear);
 
-const projectionMatrix = [
+const projectionLocation = gl.getUniformLocation(program, 'projection');
+gl.uniformMatrix4fv(projectionLocation, false, new Float32Array([
     a, 0, 0, 0,
     0, b, 0, 0,
     0, 0, c, 0,
     -1, -1, tz, 1
-];
-const projectionLocation = gl.getUniformLocation(program, 'projection');
-gl.uniformMatrix4fv(projectionLocation, false, new Float32Array(projectionMatrix));
+]));
+
+
+const quadBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, quadBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    -1.0, -1.0, 0.0, 1.0,
+    -1.0, 1.0, 0.0, 0.0,
+    1.0, -1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0, 0.0
+]), gl.STATIC_DRAW);
+
+const quadLocation = gl.getAttribLocation(program, 'quad');
+gl.vertexAttribPointer(quadLocation, 4, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(quadLocation);
+ext.vertexAttribDivisorANGLE(quadLocation, 0);
+
+
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, Float32Array.BYTES_PER_ELEMENT * MAX_ELEMENTS, gl.STATIC_DRAW);
+
+const positionLocation = gl.getAttribLocation(program, 'position');
+gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(positionLocation);
+ext.vertexAttribDivisorANGLE(positionLocation, 1);
+
+const colorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, Float32Array.BYTES_PER_ELEMENT * MAX_ELEMENTS, gl.STATIC_DRAW);
+
+const colorLocation = gl.getAttribLocation(program, 'color');
+gl.vertexAttribPointer(colorLocation, 4, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(colorLocation);
+ext.vertexAttribDivisorANGLE(colorLocation, 1);
+
+const transformsBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, transformsBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, Float32Array.BYTES_PER_ELEMENT * MAX_ELEMENTS, gl.STATIC_DRAW);
+
+const transformsLocation = gl.getAttribLocation(program, 'xforms');
+gl.vertexAttribPointer(transformsLocation, 2, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(transformsLocation);
+ext.vertexAttribDivisorANGLE(transformsLocation, 1);
+
+const dimensionsBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, dimensionsBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, Float32Array.BYTES_PER_ELEMENT * MAX_ELEMENTS, gl.STATIC_DRAW);
+
+const dimensionsLocation = gl.getAttribLocation(program, 'dimensions');
+gl.vertexAttribPointer(dimensionsLocation, 2, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(dimensionsLocation);
+ext.vertexAttribDivisorANGLE(dimensionsLocation, 1);
+
+const subImageBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, subImageBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, Float32Array.BYTES_PER_ELEMENT * MAX_ELEMENTS, gl.STATIC_DRAW);
+
+const subImageLocation = gl.getAttribLocation(program, 'subImage');
+gl.vertexAttribPointer(subImageLocation, 4, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(subImageLocation);
+ext.vertexAttribDivisorANGLE(subImageLocation, 1);
+
+const texture = gl.createTexture();
+gl.activeTexture(gl.TEXTURE0);
+gl.bindTexture(gl.TEXTURE_2D, texture);
+
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+const texLocation = gl.getUniformLocation(program, 'tex');
+gl.uniform1i(texLocation, 0);
